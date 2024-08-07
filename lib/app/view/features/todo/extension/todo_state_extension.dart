f0b4613e45aa0c36632af449702e31fb53245665
Prasant <prasant10050@gmail.com>
2024-08-08 01:42:55 +0530

    feat(bloc): Introduce mapOrNull extension

--- lib/app/view/features/todo/extension/todo_state_extension.dart
+++ lib/app/view/features/todo/extension/todo_state_extension.dart
@@ -63,23 +63,22 @@ extension TodoStateX on TodoState {
 
   // mayBeMap
   T mayBeMap<T extends Object>({
-    required T Function(TodoLoading)? loading,
-    required T Function(TodoProcessing)? processing,
-    required T Function(TodoEmpty)? empty,
-    required T Function(TodoFailure)? error,
-    required T Function(RemoveTodoState)? remove,
-    required T Function(RemoveAllTodoState)? removeAll,
-    required T Function(GetTodoState)? getById,
-    required T Function(GetAllTodoState)? getAll,
-    required T Function(AddTodoSuccess)? addTodo,
-    required T Function(UpdateTodoState)? updateTodo,
-    required T Function(MarkTodoState)? markAsDone,
-    required T Function(FilterTodoState)? filterAll,
-    required T Function(OpenAddTodoDialogState)? openAddTodoDialog,
-    required T Function(DiscardTodoDialogState)? discardTodo,
-    required T Function(OpenEditTodoDialogState)? openEditTodoDialog,
-    required T Function(ShowTodoDetailsState)? todoDetails,
-    required T Function() orElse,
+    required T Function() orElse, T Function(TodoLoading)? loading,
+    T Function(TodoProcessing)? processing,
+    T Function(TodoEmpty)? empty,
+    T Function(TodoFailure)? error,
+    T Function(RemoveTodoState)? remove,
+    T Function(RemoveAllTodoState)? removeAll,
+    T Function(GetTodoState)? getById,
+    T Function(GetAllTodoState)? getAll,
+    T Function(AddTodoSuccess)? addTodo,
+    T Function(UpdateTodoState)? updateTodo,
+    T Function(MarkTodoState)? markAsDone,
+    T Function(FilterTodoState)? filterAll,
+    T Function(OpenAddTodoDialogState)? openAddTodoDialog,
+    T Function(DiscardTodoDialogState)? discardTodo,
+    T Function(OpenEditTodoDialogState)? openEditTodoDialog,
+    T Function(ShowTodoDetailsState)? todoDetails,
   }) {
     if (this is TodoLoading) {
       return loading != null ? loading(this as TodoLoading) : orElse();
@@ -127,6 +126,65 @@ extension TodoStateX on TodoState {
           : orElse();
     } else {
       return orElse();
+    }
+  }
+
+  T? mapOrNull<T extends Object>({
+    T Function(TodoLoading)? loading,
+    T Function(TodoProcessing)? processing,
+    T Function(TodoEmpty)? empty,
+    T Function(TodoFailure)? error,
+    T Function(RemoveTodoState)? remove,
+    T Function(RemoveAllTodoState)? removeAll,
+    T Function(GetTodoState)? getById,
+    T Function(GetAllTodoState)? getAll,
+    T Function(AddTodoSuccess)? addTodo,
+    T Function(UpdateTodoState)? updateTodo,
+    T Function(MarkTodoState)? markAsDone,
+    T Function(FilterTodoState)? filterAll,
+    T Function(OpenAddTodoDialogState)? openAddTodoDialog,
+    T Function(DiscardTodoDialogState)? discardTodo,
+    T Function(OpenEditTodoDialogState)? openEditTodoDialog,
+    T Function(ShowTodoDetailsState)? todoDetails,
+  }) {
+    if (this is TodoLoading) {
+      return loading?.call(this as TodoLoading);
+    }
+    //
+    else if (this is TodoProcessing) {
+      return processing?.call(this as TodoProcessing);
+    } else if (this is TodoFailure) {
+      return error?.call(this as TodoFailure);
+    } else if (this is TodoEmpty) {
+      return empty?.call(this as TodoEmpty);
+    } else if (this is RemoveTodoState) {
+      return remove?.call(this as RemoveTodoState);
+    }
+    //
+    else if (this is RemoveAllTodoState) {
+      return removeAll?.call(this as RemoveAllTodoState);
+    } else if (this is GetTodoState) {
+      return getById?.call(this as GetTodoState);
+    } else if (this is GetAllTodoState) {
+      return getAll?.call(this as GetAllTodoState);
+    } else if (this is AddTodoSuccess) {
+      return addTodo?.call(this as AddTodoSuccess);
+    } else if (this is UpdateTodoState) {
+      return updateTodo?.call(this as UpdateTodoState);
+    } else if (this is MarkTodoState) {
+      return markAsDone?.call(this as MarkTodoState);
+    } else if (this is FilterTodoState) {
+      return filterAll?.call(this as FilterTodoState);
+    } else if (this is OpenAddTodoDialogState) {
+      return openAddTodoDialog?.call(this as OpenAddTodoDialogState);
+    } else if (this is DiscardTodoDialogState) {
+      return discardTodo?.call(this as DiscardTodoDialogState);
+    } else if (this is OpenEditTodoDialogState) {
+      return openEditTodoDialog?.call(this as OpenEditTodoDialogState);
+    } else if (this is ShowTodoDetailsState) {
+      return todoDetails?.call(this as ShowTodoDetailsState);
+    } else {
+      return null;
     }
   }
 }
